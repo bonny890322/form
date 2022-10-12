64a65ee13acf657882942f39a8edd12009c68d58
dai322 <C107118248@nkust.edu.tw>
2022-10-12 20:12:49 +0800

    feat: new form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'form';
+
+  addForm!: FormGroup;
+  today = new Date
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.addForm = this.fb.group({
+      date_for_estimated_completion: ['', [Validators.required]],
+      remark: ['', [Validators.required]],
+      user_id: ['', [Validators.required]],
+      creater: [''],
+      countersign_id: [''],
+    });
+
+    const today = new Date();
+  }
+
+  ngOnInit(): void {
+
+    this.addForm.patchValue({
+      date_for_estimated_completion: this.today,
+    });
+  }
+
+  reset() {
+    this.addForm.reset()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,80 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTreeModule } from '@angular/material/tree';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { DatePipe } from '@angular/common';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const MaModule = [
+  FormsModule,
+  ReactiveFormsModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatSnackBarModule,
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatTabsModule,
+  MatExpansionModule,
+  MatTableModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSlideToggleModule,
+  MatRippleModule,
+  MatTreeModule,
+  CdkTreeModule
+  //HttpClientInMemoryWebApiModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
